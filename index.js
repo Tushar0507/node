4e42f8aed97f7b12541e39041f9b8b48e2f76bb4
Tushar jaiswal <67146936+Tushar0507@users.noreply.github.com>
2020-08-31 00:51:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var mysqlConnection = mysql.createConnection({
 });
 
 mysqlConnection.connect((err) => {
-  if (!err) console.log("DB connection succeded.");
+  if (!err) console.log("DB connection suceded.");
   else
     console.log(
       "DB connection failed \n Error : " + JSON.stringify(err, undefined, 2)
